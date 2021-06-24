886944c4b426955ec3ca9b0cb873402c3e6b0b83
dattatray bedagkar <dattatray.bedagkar@gmail.com>
2021-06-24 14:53:39 +0530

    py file added

---
+++ week02day01 assignment.py
@@ -0,0 +1,37 @@
+#python introduction 
+'''
+Question 1: Play with repl while running commands and operators.
+ans -       repl stands for read evaluate print loop.
+1)read     -The read function accepts an expression from the user, and 
+             parses it into a data structure in memory.
+2)evaluate -The eval function takes this internal data structure and 
+             evaluates it .
+3)print    -The print function takes the result yielded by eval, and 
+             prints it out to the user.
+4)loop     -until desired output is reached the above functions are 
+             repeated again and again with loop function.
+
+ Question 2: Given a no print its remainder. Which command will you use ? 
+ ans-        To find the remainder for the given divison we use the modulus
+              operator % .the example of the same is given below.                             
+'''
+a=int(input("enter 1st number "))
+b=int(input("enter 2nd number "))
+c=a%b
+print(c)
+'''
+3) Experiment with str concatenation.
+The strcat function is used to 
+concatenate one string (source) at the 
+end of another string (destination).its functions are
+
+Takes the destination string.
+Finds the NULL character.
+Copies the source string starting at the location of the 
+NULL character of destination string.
+Appends a NULL character to the destination string when 
+all characters of the source string are copied to the destination string.
+
+remaining 2 questions are practical oriented hence i have not included them in
+this assignment.
+'''
